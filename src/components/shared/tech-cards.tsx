3ef46f809b705868d103d48ced75e878ea349446
khalidkhankakar <khalidkhankakar2468@gmail.com>
2025-09-22 16:38:02 +0500

    add tech section

--- src/components/shared/tech-cards.tsx
+++ src/components/shared/tech-cards.tsx
@@ -5,44 +5,48 @@ import { InfiniteMovingCards } from "../ui/infinite-moving-cards";
 
 export function TechCards() {
   return (
-    <div className=" rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+    <div className="rounded-md flex flex-col w-full mb-28 items-center justify-center relative overflow-hidden">
       <InfiniteMovingCards
-        items={testimonials}
+        items={techCards}
         direction="right"
-        speed="slow"
+        speed="fast"
+        pauseOnHover={false}
       />
     </div>
   );
 }
 
-const testimonials = [
+const techCards = [
   {
-    quote:
-      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
-    name: "Charles Dickens",
-    title: "A Tale of Two Cities",
+    name:'Javascript',
+    src:'/tech-icons/javascript.svg'
   },
   {
-    quote:
-      "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
-    name: "William Shakespeare",
-    title: "Hamlet",
+    name:'TypeScript',
+    src:'/tech-icons/typescript.svg'
   },
   {
-    quote: "All that we see or seem is but a dream within a dream.",
-    name: "Edgar Allan Poe",
-    title: "A Dream Within a Dream",
+    name:'ReactJs',
+    src:'/tech-icons/reactjs.svg'
   },
   {
-    quote:
-      "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
-    name: "Jane Austen",
-    title: "Pride and Prejudice",
+    name:'NextJs',
+    src:'/tech-icons/nextjs.svg'
   },
   {
-    quote:
-      "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
-    name: "Herman Melville",
-    title: "Moby-Dick",
+    name:'CSS',
+    src:'/tech-icons/css.svg'
   },
-];
+  {
+    name:'Eslint',
+    src:'/tech-icons/eslint.svg'
+  },
+  {
+    name:'GSAP',
+    src:'/tech-icons/gsap.svg'
+  },
+  {
+    name:'MySQL',
+    src:'/tech-icons/mysql.svg'
+  }
+]
--- src/components/ui/infinite-moving-cards.tsx
+++ src/components/ui/infinite-moving-cards.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
 export const InfiniteMovingCards = ({
@@ -11,9 +12,8 @@ export const InfiniteMovingCards = ({
   className,
 }: {
   items: {
-    quote: string;
-    name: string;
-    title: string;
+    src:string,
+    name:string
   }[];
   direction?: "left" | "right";
   speed?: "fast" | "normal" | "slow";
@@ -77,41 +77,18 @@ export const InfiniteMovingCards = ({
         className,
       )}
     >
-      <ul
+      <div
         ref={scrollerRef}
         className={cn(
-          "flex w-max min-w-full shrink-0 flex-nowrap gap-4 py-4",
+          "flex  w-max min-w-full shrink-0 flex-nowrap  gap-8 py-2",
           start && "animate-scroll",
           pauseOnHover && "hover:[animation-play-state:paused]",
         )}
       >
-        {items.map((item, idx) => (
-          <li
-            className="relative w-[350px] max-w-full shrink-0 rounded-2xl border border-b-0 border-zinc-200 bg-[linear-gradient(180deg,#fafafa,#f5f5f5)] px-8 py-6 md:w-[450px] dark:border-zinc-700 dark:bg-[linear-gradient(180deg,#27272a,#18181b)]"
-            key={item.name}
-          >
-            <blockquote>
-              <div
-                aria-hidden="true"
-                className="user-select-none pointer-events-none absolute -top-0.5 -left-0.5 -z-1 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
-              ></div>
-              <span className="relative z-20 text-sm leading-[1.6] font-normal text-neutral-800 dark:text-gray-100">
-                {item.quote}
-              </span>
-              <div className="relative z-20 mt-6 flex flex-row items-center">
-                <span className="flex flex-col gap-1">
-                  <span className="text-sm leading-[1.6] font-normal text-neutral-500 dark:text-gray-400">
-                    {item.name}
-                  </span>
-                  <span className="text-sm leading-[1.6] font-normal text-neutral-500 dark:text-gray-400">
-                    {item.title}
-                  </span>
-                </span>
-              </div>
-            </blockquote>
-          </li>
+        {items.map((item) => (
+          <Image key={item.src} src={item.src} width={80} height={60} alt={item.name} className="grayscale-100 hover:grayscale-0 transition-all cursor-pointer object-contain w-16 h-16"   />
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
