7bc376722d616abe3a9a952f8dbf10f3d40030e9
khalidkhankakar <khalidkhankakar2468@gmail.com>
2025-10-17 21:56:47 +0500

    setup drizzle orm

---
+++ src/drizzle/db.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+
+const sql = neon(process.env.DATABASE_URL!);
+
+export const db = drizzle({
+  logger: true,
+  client: sql,
+});
